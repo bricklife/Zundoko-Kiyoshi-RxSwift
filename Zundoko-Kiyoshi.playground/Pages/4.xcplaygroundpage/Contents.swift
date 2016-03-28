ebbc64b771e715544ba87631498f055890e97751
Shinichiro Oba <ooba@bricklife.com>
2016-03-29 00:56:00 +0900

    Add a synchronous sample

---
+++ Zundoko-Kiyoshi.playground/Pages/4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+import RxSwift
+
+enum ズンドコ: UInt32 {
+    case ズン
+    case ドコ
+}
+
+let ズンドコ川 = Observable<Int>.interval(0.5, scheduler: MainScheduler.instance)
+    .map { _ in ズンドコ(rawValue: arc4random_uniform(2))! }
+    .shareReplay(1)
+
+let キヨシトリガー = [ズンドコ川, ズンドコ川.skip(1), ズンドコ川.skip(2), ズンドコ川.skip(3), ズンドコ川.skip(4)]
+    .zip { $0 }
+    .skipWhile { $0 != [.ズン, .ズン, .ズン, .ズン, .ドコ] }
+    .skip(1)
+
+_ = ズンドコ川
+    .withLatestFrom(キヨシトリガー.map { _ in false }.startWith(true)) { $0 }
+    .takeWhile { $0.1 }
+    .map { $0.0 }
+    .subscribe(onNext: { print($0) }, onCompleted: { print("キ・ヨ・シ！") })
+
+///
+
+import XCPlayground
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
